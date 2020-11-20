1e566ae192eea9aa915b5babf3589140757b4cb2
Bilal Aoun <Bilal.Aoun@rockets.utoledo.edu>
2020-11-20 04:56:07 -0500

    Second Commit

---
+++ HelloW/HelloWorldclass.java
@@ -0,0 +1,11 @@
+package HelloW;
+
+public class HelloWorldclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world, this is Bilal");
+
+	}
+
+}
